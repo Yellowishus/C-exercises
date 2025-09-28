5889ebc0e4e7d0e0e9e593527401810be123c149
Maciej <125089432+Yellowishus@users.noreply.github.com>
2025-09-28 20:32:37 +0200

    feat: made logic for all the game
    also made new function handling messages

--- Rock-paper-scizors/Rock-paper-scizors/Program.cs
+++ Rock-paper-scizors/Rock-paper-scizors/Program.cs
@@ -1,31 +1,99 @@
 ﻿// See https://aka.ms/new-console-template for more information
 try
 {
-    Console.WriteLine("Hello, World!");
     Console.WriteLine(RPSGame());
 }
 catch (FormatException fEm)
 {
-    Console.WriteLine( fEm.Message );
+    Console.WriteLine(fEm.Message);
 }
 string RPSGame()
 {
     Random rnd = new Random();
     int oponentShape = rnd.Next(3); //oponentShape is one of three posible shapes 0=rock 1=paper 2=scisors 
-    Console.WriteLine(oponentShape);
     Console.WriteLine("Lets play Rock-Paper-Scisors game:");
     Console.WriteLine("Please type your choice");
     string shape = Console.ReadLine();
-    switch (shape)
+    if (shape == "rock")
     {
-        case "rock":
-            return "rock";
-        case "paper":
-            return "paper";
-        case "scisors":
-            return "scisors";
-        default:
-            return "Sorry i didint recognised your word. Before trying again make sure you wrote it without using capital-letters.";
+        if (oponentShape == 2)
+        {
+            return messages("victory");
+        }
+        else if (oponentShape == 1)
+        {
+            return messages("defeat");
+        }
+        else if (oponentShape == 0)
+        {
+            return messages("draw");
+        }
+        else
+        {
+            return "error";
+        }
     }
-    
+    else if (shape == "paper")
+    {
+        if (oponentShape == 0)
+        {
+            return messages("victory");
+        }
+        else if (oponentShape == 2)
+        {
+            return messages("defeat");
+        }
+        else if (oponentShape == 1)
+        {
+            return messages("draw");
+        }
+        else
+        {
+            return "error";
+        }
+    }
+    else if (shape == "scisors")
+    {
+        if (oponentShape == 1)
+        {
+            return messages("victory");
+        }
+        else if (oponentShape == 0)
+        {
+            return messages("defeat");
+        }
+        else if (oponentShape == 2)
+        {
+            return messages("draw");
+        }
+        else
+        {
+            return "error";
+        }
+    }
+    else
+    {
+        return "error";
+    }
+
+}
+string messages(string result)
+{
+    string victoryMessage = "victory";
+    string defeatMessage = "defeat";
+    string drawMessage = "draw";
+    string errorMessage = "error";
+    if (result == "victory")
+    {
+        return victoryMessage;
+    }
+    else if (result == "defeat")
+    {
+        return defeatMessage;
+    }
+    else if (result == "draw")
+    {
+        return drawMessage;
+    }
+    else return errorMessage;
 }
